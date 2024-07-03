e5e89fa92971c729086811296c209ee1395a6c05
DairaCavo <167671115+DairaCavo@users.noreply.github.com>
2024-07-03 04:16:54 -0300

    Add files via upload

---
+++ copaamerica/informacion.php
@@ -0,0 +1,9 @@
+<html lang="es" class="informacion">
+    <head>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<?php
+include('estilos.css');
+echo "<p>Informacion adiccional de las copas americas pasadas </p>"
+?>
+</html>
